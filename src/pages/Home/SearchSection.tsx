f979d1a608cd4ece97af214b9267f68176fd1f2a
phongnhat19 <phongnhat19@gmail.com>
2020-10-04 17:05:23 +0700

    handling responsive for search bar

--- src/pages/Home/SearchSection.tsx
+++ src/pages/Home/SearchSection.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Button, Icon, Input } from 'rsuite';
+import { useViewport } from '../../context/ViewportContexrt';
 
 const SearchSection = () => {
+    const {isMobile} = useViewport();
     return (
         <div className="search-section">
             <div className="search-input-wrapper">
-                <Input size="lg" placeholder="Search by Address / TxHash / BlockHash ..." />
-                <Button appearance="primary">
-                    <Icon icon="search"  /> Search
-                </Button>
+                <Input block size="lg" placeholder="Search by Address / TxHash / BlockHash ..." />
+                {
+                    isMobile ? 
+                    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: '15px'}}>
+                        <div style={{width: '47%'}}>
+                            <Button appearance="primary" block>
+                                <Icon icon="search"  /> Search
+                            </Button>
+                        </div>
+                        <div style={{width: '47%'}}>
+                            <Button appearance="ghost" block>
+                                <Icon icon="qrcode"  /> Scan QR code
+                            </Button>
+                        </div>
+                    </div>
+                    :
+                    <Button block appearance="primary">
+                        <Icon icon="search"  /> Search
+                    </Button>
+                }
             </div>
         </div>
     )
