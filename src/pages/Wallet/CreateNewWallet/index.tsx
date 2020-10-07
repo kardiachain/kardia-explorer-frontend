5fd6e47975d9e95bee14996b6fad379a1569ee3d
phongnhat19 <phongnhat19@gmail.com>
2020-10-07 13:51:30 +0700

    Remove mnemonic tab

--- src/pages/Wallet/CreateNewWallet/index.tsx
+++ src/pages/Wallet/CreateNewWallet/index.tsx
@@ -1,14 +1,12 @@
 import  React, { useState } from 'react';
 import { Col, FlexboxGrid, Nav, Panel } from 'rsuite';
 import CreateByKeystore from './CreateByKeystore';
-import CreateByMnemonicPhrase from './CreateByMnemonicPhrase';
 import CreateByPrivateKey from './CreateByPrivateKey';
 import './createWallet.css'
 
 const CreateNewWallet = () => {
 
     const [activeKey, setActiveKey] = useState("private-key");
-
 
     return (
         <div className="create-wallet-container">
@@ -17,13 +15,12 @@ const CreateNewWallet = () => {
                     <FlexboxGrid.Item componentClass={Col} colspan={22} md={14}>
                         <Panel shaded>
                             <Nav appearance="tabs" justified onSelect={setActiveKey}  >
-                                <Nav.Item eventKey="private-key" active={ activeKey === "private-key" ? true : false}>Create by private key</Nav.Item>
-                                <Nav.Item eventKey="keystore" active={ activeKey === "keystore" ? true : false}>Create by keystore file</Nav.Item>
-                                <Nav.Item eventKey="mnemonic" active={ activeKey === "mnemonic" ? true : false}>Create by mnemonic phrase</Nav.Item>
+                                <Nav.Item eventKey="private-key" active={ activeKey === "private-key" }>Create by private key</Nav.Item>
+                                <Nav.Item eventKey="keystore" active={ activeKey === "keystore" }>Create by keystore file</Nav.Item>
                             </Nav>
                             <div className="child-tab">
                                 {
-                                    activeKey === "private-key" ? <CreateByPrivateKey /> : activeKey === "keystore" ? <CreateByKeystore /> : <CreateByMnemonicPhrase />
+                                    activeKey === "private-key" ? <CreateByPrivateKey /> : <CreateByKeystore />
                                 }
                             </div>
                         </Panel>
