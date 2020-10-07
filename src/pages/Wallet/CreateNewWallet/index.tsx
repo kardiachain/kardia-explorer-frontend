b76a5368a045e865cd9fcf041e841ef8a7b33010
phongnhat19 <phongnhat19@gmail.com>
2020-10-07 11:14:01 +0700

    Relint source code

--- src/pages/Wallet/CreateNewWallet/index.tsx
+++ src/pages/Wallet/CreateNewWallet/index.tsx
@@ -1,5 +1,5 @@
 import  React, { useState } from 'react';
-import { Col, FlexboxGrid, Icon, Nav, Panel } from 'rsuite';
+import { Col, FlexboxGrid, Nav, Panel } from 'rsuite';
 import CreateByKeystore from './CreateByKeystore';
 import CreateByMnemonicPhrase from './CreateByMnemonicPhrase';
 import CreateByPrivateKey from './CreateByPrivateKey';
@@ -9,31 +9,28 @@ const CreateNewWallet = () => {
 
     const [activeKey, setActiveKey] = useState(0);
 
-    const handleSelect = (activeK: any) => {
-        setActiveKey(activeK);
-    }
 
     return (
-            <div className="create-wallet-container">
-                <div className="show-grid">
-                    <FlexboxGrid justify="center">
-                        <FlexboxGrid.Item componentClass={Col} colspan={22} md={14}>
-                            <Panel shaded>
-                                <Nav appearance="tabs" justified onSelect={(eventKey) => handleSelect(eventKey)}  >
-                                    <Nav.Item eventKey="0" active={ activeKey == 0 ? true : false}>Create by private key</Nav.Item>
-                                    <Nav.Item eventKey="1" active={ activeKey == 1 ? true : false}>Create by keystore file</Nav.Item>
-                                    <Nav.Item eventKey="2" active={ activeKey == 2 ? true : false}>Create by mnemonic phrase</Nav.Item>
-                                </Nav>
-                                <div className="child-tab">
-                                    {
-                                        activeKey == 0 ? <CreateByPrivateKey /> : activeKey == 1 ? <CreateByKeystore /> : <CreateByMnemonicPhrase />
-                                    }
-                                </div>
-                            </Panel>
-                        </FlexboxGrid.Item>
-                    </FlexboxGrid>
-                </div>
+        <div className="create-wallet-container">
+            <div className="show-grid">
+                <FlexboxGrid justify="center">
+                    <FlexboxGrid.Item componentClass={Col} colspan={22} md={14}>
+                        <Panel shaded>
+                            <Nav appearance="tabs" justified onSelect={setActiveKey}  >
+                                <Nav.Item eventKey="0" active={ activeKey === 0 ? true : false}>Create by private key</Nav.Item>
+                                <Nav.Item eventKey="1" active={ activeKey === 1 ? true : false}>Create by keystore file</Nav.Item>
+                                <Nav.Item eventKey="2" active={ activeKey === 2 ? true : false}>Create by mnemonic phrase</Nav.Item>
+                            </Nav>
+                            <div className="child-tab">
+                                {
+                                    activeKey === 0 ? <CreateByPrivateKey /> : activeKey === 1 ? <CreateByKeystore /> : <CreateByMnemonicPhrase />
+                                }
+                            </div>
+                        </Panel>
+                    </FlexboxGrid.Item>
+                </FlexboxGrid>
             </div>
+        </div>
     );
 }
 
