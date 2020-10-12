2ef705cf79f5cd7d47757b0c37b9254d83956a65
phidangnguyen <phidang0210@gmail.com>
2020-10-12 23:26:03 +0700

    create wallet with keystore

--- src/pages/Wallet/CreateNewWallet/CreateByKeystore.tsx
+++ src/pages/Wallet/CreateNewWallet/CreateByKeystore.tsx
@@ -1,50 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, ButtonToolbar, FlexboxGrid, Form, FormControl, FormGroup } from 'rsuite';
 import Wallet from 'ethereumjs-wallet'
+import { useWalletStorage } from '../../../store/wallet';
+import { useHistory } from 'react-router-dom';
 
 const CreateByKeystore = () => {
     const [password, setPassword] = useState('');
     const [blobUrl, setBlobUrl] = useState('')
     const [keystoreFilename, setKeystoreFilename] = useState('')
-
+    const [isLoading, setLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('')
+    const [walletStored, setWalletStored] = useWalletStorage()
+    const [privateKey, setPrivateKey] = useState('')
+    let history = useHistory();
     const createWallet = async () => {
-        if(!password) return;
+        if(!password) {
+            setErrorMessage('This field is required')
+            return;
+        }
+        setLoading(true)
         // Generate wallet
         const wallet = Wallet.generate();
-        const publicKey = wallet.getPublicKeyString();
-        const walletAddr = wallet.getAddressString()
+        setPrivateKey(wallet.getPrivateKeyString())
         const keystoreFilename = wallet.getV3Filename();
-
         const keystoreJson = await wallet.toV3(password);
-
         const keystoreJsonString = JSON.stringify(keystoreJson);
         const keystoreBlob = new Blob([keystoreJsonString], {
           type: 'mime',
         });
         setBlobUrl(window.URL.createObjectURL(keystoreBlob));
         setKeystoreFilename(keystoreFilename);
+        setLoading(false)
     }
 
-    return (
-        <div className="show-grid creact-by-privatekey">
+    const accessWallet = async() => {
+        if(!privateKey) return;
+        setWalletStored({privatekey: privateKey, isAccess: true})
+        history.push("/dashboard");
+    }
+
+    useEffect(() => {
+        if(password) {
+            setErrorMessage('')
+        }
+    }, [password]);    
+
+    return !blobUrl ? (
+        <div className="show-grid creact-by-keystore">
             <FlexboxGrid justify="start">
                 <div className="note-warning">
                     <Form fluid>
                         <FormGroup>
-                            <FormControl placeholder="Password*" name="password" type="password" value={password} onChange={setPassword} />
+                            <FormControl errorMessage={errorMessage} placeholder="Password*" name="password" type="password" value={password} onChange={setPassword} />
                         </FormGroup>
                         <div>You will need <b>BOTH</b> your <b>Password + Keystore File</b> to access your wallet.</div>
                         <FormGroup>
                             <ButtonToolbar>
-                                <Button appearance="primary" onClick={createWallet}>Create wallet</Button>
+                                <Button appearance="primary" loading={isLoading} onClick={createWallet}>Create wallet</Button>
                             </ButtonToolbar>
                         </FormGroup>
                     </Form>
                 </div>
             </FlexboxGrid>
-            {/* <a href={blobUrl} download={keystoreFilename}>
-                Download Keystore File
-            </a> */}
+        </div>
+    ) : (
+        <div className="show-grid creact-by-keystore">
+            <FlexboxGrid justify="start">
+                <div className="note-warning">
+                <div>Please download and save the following Keystore File. You will need it and your password to access your wallet.</div>
+                <div className="download-keystore-file">
+                    <a href={blobUrl} download={keystoreFilename}>
+                        Download Keystore File
+                    </a>
+                </div>
+                <ButtonToolbar>
+                    <Button appearance="primary" onClick={accessWallet}>Access now</Button>
+                </ButtonToolbar>
+                </div>
+            </FlexboxGrid>
         </div>
     );
 }
