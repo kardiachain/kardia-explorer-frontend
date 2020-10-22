1a65f6c718a018a6bb531ad12e0f3954423f677e
phidangnguyen <phidang0210@gmail.com>
2020-10-22 20:38:59 +0700

    interval get transaction

--- src/pages/Home/TransactionSection.tsx
+++ src/pages/Home/TransactionSection.tsx
@@ -14,10 +14,10 @@ const TransactionSection = () => {
     const { isMobile } = useViewport();
     const history = useHistory();
     useEffect(() => {
-        (async () => {
+        setInterval(async () => {
             const rs = await getTransactions(TABLE_CONFIG.page, TABLE_CONFIG.limitDefault);
             setTransactionList(rs.transactions)
-        })()
+        }, 2000)
     }, [])
     return (
         <Panel header="Latest transactions" shaded>
@@ -52,7 +52,7 @@ const TransactionSection = () => {
                                 {(rowData: KAITransaction) => {
                                     return (
                                         <div>
-                                            <div style={{marginBottom: '5px'}}>From: {renderHashToRedirect(rowData.from, isMobile ? 10 : 30, () => { history.push(`/txs?addresses=${rowData.from}`) })} </div>
+                                            <div style={{ marginBottom: '5px' }}>From: {renderHashToRedirect(rowData.from, isMobile ? 10 : 30, () => { history.push(`/txs?addresses=${rowData.from}`) })} </div>
                                             <div>To: {renderHashToRedirect(rowData.to, isMobile ? 10 : 30, () => { history.push(`/txs?addresses=${rowData.to}`) })}</div>
                                         </div>
                                     );
