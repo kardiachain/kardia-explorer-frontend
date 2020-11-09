6b5772af9e6b44ce990f535201f53034233dbd1f
phongnhat19 <phongnhat19@gmail.com>
2020-11-09 18:31:46 +0700

    hot fix

--- src/common/utils/string.tsx
+++ src/common/utils/string.tsx
@@ -58,19 +58,24 @@ const renderHashStringAndTooltip = (hash: string, headCount?: number, tailCount?
 }
 
 const renderHashToRedirect = ({
-    hash, headCount = 6, tailCount = 4, showTooltip = true, callback
+    hash, headCount = 6, tailCount = 4, showTooltip = true, callback, showCopy = true
 }: {
-    hash: any, headCount?: number, tailCount?: number, showTooltip?: boolean, callback?: () => void
+    hash: any, headCount?: number, tailCount?: number, showTooltip?: boolean, callback?: () => void, showCopy?: boolean
 }) => {
+    const onSuccess = () => {
+        Alert.success('Copied to clipboard.')
+    }
     return showTooltip ? (
         <Whisper placement="autoVertical" trigger="hover" speaker={<Tooltip className="custom-tooltip">{hash}</Tooltip>}>
-            <span className="hex" onClick={() => {callback && callback()}}  style={{ cursor: 'pointer', color:'#1f0080' }}>
-                {truncate(String(hash), headCount, tailCount)}{' '}
+            <span className="hex" style={{ cursor: 'pointer', color:'#9E3144' }}>
+                <span onClick={() => {callback && callback()}}>{truncate(String(hash), headCount, tailCount)}{' '}</span>
+                {showCopy && renderCopyButton({str: hash, size: "xs", callback: () => copyToClipboard(hash, onSuccess)})}
             </span>
         </Whisper>
     ) : (
-        <span className="hex" onClick={() => {callback && callback()}}  style={{ cursor: 'pointer', color:'#1f0080' }}>
-            {truncate(String(hash), headCount, tailCount)}{' '}
+        <span className="hex" style={{ cursor: 'pointer', color:'#9E3144' }}>
+            <span onClick={() => {callback && callback()}} >{truncate(String(hash), headCount, tailCount)}{' '}</span>
+            {showCopy && renderCopyButton({str: hash, size: "xs", callback: () => copyToClipboard(hash, onSuccess)})}
         </span>
     )
 }
