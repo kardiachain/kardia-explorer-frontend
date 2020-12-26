b44fd30fddd079f4fd8314b4bcfdbe0443dc150c
phidangnguyen <phidang0210@gmail.com>
2020-12-26 22:27:29 +0700

    update padding home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -68,20 +68,14 @@ const Home = () => {
     return (
         <React.Fragment>
             <div className="container home-container">
-
                 <SearchSection />
-
-                <div className="home-top-section">
-                    <FlexboxGrid justify="space-between" style={{ flexFlow: 'row', alignItems: 'normal' }}>
-                        <FlexboxGrid.Item componentClass={Col} colspan={24} md={12} sm={24}
-                            style={{
-                                background: 'white',
-                                padding: 20
-                            }}>
+                <div>
+                    <FlexboxGrid className="home-top-section" justify="space-between" style={{ alignItems: 'normal' }}>
+                        <FlexboxGrid.Item componentClass={Col} colspan={24} md={12} sm={24} style={{marginRight: !isMobile ? 5 : 0}} className="wrap-token bocktime-chart-container">
                             <BlockTimeChart blockList={blocksForChart} />
                         </FlexboxGrid.Item>
 
-                        <FlexboxGrid.Item componentClass={Col} colspan={24} md={12} sm={24} className="wrap-token">
+                        <FlexboxGrid.Item componentClass={Col} colspan={24} md={12} sm={24} style={{marginLeft: !isMobile ? 5 : 0}} className="wrap-token">
                             <Panel shaded className="_wrap">
                                 <div className="token-infor">
                                     <div className="left">
@@ -146,14 +140,14 @@ const Home = () => {
                     <StatsSection totalTxs={totalTxs} blockHeight={blockHeight} blockList={tpsCalculateBlocks} />
                 </FlexboxGrid>
 
-                <FlexboxGrid justify="space-between" style={{ marginTop: !isMobile ? '30px' : '0' }}>
+                <FlexboxGrid justify="space-between">
                     <FlexboxGrid.Item className="section-left" componentClass={Col} colspan={24} md={12} sm={24} style={{ marginBottom: '20px' }}>
                         <div className="block-title">
                             <div style={{ display: 'flex', alignItems: 'center' }}>
                                 <Icon className="highlight" icon="th-large" size={"lg"} />
                                 <p style={{ marginLeft: '12px' }} className="title">Latest Blocks</p>
                             </div>
-                            <Button onClick={() => { history.push('/blocks') }}>View all</Button>
+                            <Button onClick={() => { history.push('/blocks') }} style={{marginRight: 0}}>View all</Button>
                         </div>
                         <BlockSection blockList={blocks} />
                     </FlexboxGrid.Item>
@@ -163,7 +157,7 @@ const Home = () => {
                                 <Icon className="highlight" icon="exchange" size={"lg"} />
                                 <p style={{ marginLeft: '12px' }} className="title">Latest Transactions</p>
                             </div>
-                            <Button onClick={() => { history.push('/txs') }}>View all</Button>
+                            <Button onClick={() => { history.push('/txs') }} style={{marginRight: 0}}>View all</Button>
                         </div>
                         <TransactionSection transactionList={transactionList} />
                     </FlexboxGrid.Item>
