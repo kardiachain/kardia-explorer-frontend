a14efa58c7cc2c14bbb457bdac1f8a5ecd9a940a
phongnhat19 <phongnhat19@gmail.com>
2020-12-30 11:51:27 +0700

    add Sentry watcher

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://ad94a5b0038f45409f2d79b2799988f3@o495346.ingest.sentry.io/5574701",
+  autoSessionTracking: true,
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
