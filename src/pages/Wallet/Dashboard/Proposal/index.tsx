98afcb041f1b97dda8ebf24a57d03f1d9c314b70
phidangnguyen <phidang0210@gmail.com>
2021-02-02 18:07:08 +0700

    Update proposal create note

--- src/pages/Wallet/Dashboard/Proposal/index.tsx
+++ src/pages/Wallet/Dashboard/Proposal/index.tsx
@@ -187,7 +187,7 @@ const CreateProposal = () => {
                             <FlexboxGrid.Item componentClass={Col} colspan={24} md={24}>
                                 <Form fluid>
                                     <FormGroup>
-                                        <div className="proposal-note">You must lock 500,000 KAI to create proposal.</div>
+                                        <div className="proposal-note">You must lock 500,000 KAI to create a proposal. The network will refund your tokens if the proposal is approved by 2/3+ voting power, otherwise it will be slashed and locked into the Kardia Treasury.</div>
                                         <FlexboxGrid>
                                             <FlexboxGrid.Item componentClass={Col} colspan={24} md={8} xs={24} style={{ marginBottom: 15 }}>
                                                 <ControlLabel className="color-white">Gas Limit (required)</ControlLabel>
