87bef5fabb55546678906a58ab025e9eeada8eba
phidangnguyen <phidang0210@gmail.com>
2021-03-12 17:48:32 +0700

    update staking data modal

---
+++ src/service/kai-explorer/validator/type.d.ts
@@ -0,0 +1,9 @@
+export interface ValidatorStats {
+    totalValidators: number;
+    totalProposers: number;
+    totalCandidates: number;
+    totalDelegators: number;
+    totalStakedAmount: any;
+    totalValidatorStakedAmount: any;
+    totalDelegatorStakedAmount: any;
+}
