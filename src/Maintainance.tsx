ed6fabd9bde3c56fde5dbd44cbfdfc35a3cf8cf4
phongnhat19 <phongnhat19@gmail.com>
2021-05-04 11:36:29 +0700

    update maintainance template

---
+++ src/Maintainance.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './maintain.css'
+
+export default () => {
+  return (
+    <div>
+      <article>
+        <h1>We&rsquo;ll be back soon!</h1>
+        <div>
+          <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:hello@kardiachain.io">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+          <p>&mdash; KardiaChain Team</p>
+        </div>
+      </article>
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
+import Maintainance from './Maintainance';
 
 Sentry.init({
   dsn: "https://95cf929f53bd4ead85d8d918cc2e2ba5@o497910.ingest.sentry.io/5574725",
@@ -20,8 +21,7 @@ Sentry.init({
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1 style={{textAlign: 'center'}}>Under maintainance</h1>
-    <h2 style={{textAlign: 'center'}}>We will get back soon</h2>
+    <Maintainance />
     {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
